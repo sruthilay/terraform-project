27856b9cd40d36316a00db81b2f57d4826a9514a
sruthi <35577802+sruthilay@users.noreply.github.com>
2021-07-09 02:55:07 -0700

    Create sg.tf

---
+++ sg.tf
@@ -0,0 +1,25 @@
+resource "aws_security_group" "webserver_sg" {
+  vpc_id = aws_vpc.vpc_main.id
+
+  // Outgoing
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  // Incoming
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
