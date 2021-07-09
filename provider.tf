4de1e5532c3e12cf99ab3d1d9dea9fb29b0905db
sruthi <35577802+sruthilay@users.noreply.github.com>
2021-07-09 10:17:07 -0700

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,3 +2,12 @@ provider "aws" {
   region = var.region
 
 }
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.1.0"
+    }
+  }
+  required_version = ">= 0.14.9"
+}
